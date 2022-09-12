9a5d5e4d191d303fc60aaab48b5563d716092041
machunhuan0204 <104007554+machunhuan0204@users.noreply.github.com>
2022-09-12 16:50:06 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,64 +1,144 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
+
+#include <stdlib.h>
 #include <string.h>
-#include "contact.h"
-//1.存放1000个好友的信息
-// 名字  电话  性别   住址  年龄
-// 2.增加好友信息
-// 3.删除指定名字信息
-// 4.
-//
+#include <stdio.h>
+#include <errno.h>
 
-
-void menu()
+struct S
 {
-	printf("**********************************\n");
-	printf("****1.add     2.del    ***********\n");
-	printf("****3.search  4.modify ***********\n");
-	printf("****5.show    6.sort   ***********\n");
-	printf("****0.exit             ***********\n");
-	printf("**********************************\n");
-	
-}
+	int n;
+	int* arr;
+};
 int main()
 {
-	int input = 0;
-	//创建通讯录
-	struct Contact con;//con 就是通讯录   存放100个人的信息
-	//初始化通讯录
-	InitContact(&con);
-	do
+	struct S* ps = (struct S*)malloc(sizeof(struct S));
+	ps->arr = malloc(5 * sizeof(int));
+	int i = 0;
+	for (i = 0; i < 5; i++)
 	{
-		menu();
-		printf("请选择：>");
-		scanf("%d", &input);
-		switch (input)
-		{
-		case ADD:
-			AddContact(&con);
-			break;
-		case DEL:
-			DelContact(&con);
-			break;
-		case SEARCH:
-			SearchContact(&con);
-			break;
-		case MODIFY:
-			ModifyContact(&con);
-			break;
-		case SHOW:
-			ShowContact(&con);
-			break;
-		case SORT:
-			break;
-		case EXIT:
-			printf("退出通讯录");
-			break;
-		default:
-			printf("选择错误，请重新选择！！！");
-			break;
-		}
-	} while (input);
+		ps->arr[i] = i;
+	}
+	for (i = 0; i < 5; i++)
+	{
+		printf("%d ", ps->arr[i]);
+	}
+	//调整大小
+	int* ptr = realloc(ps->arr, 20 * sizeof(int));
+	if (ptr != NULL)
+	{
+		ps->arr = ptr;
+	}
+	for (i = 5; i<10;i++)
+	{
+		ps->arr[i] = i;
+	}
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", ps->arr[i]);
+	}
+	//释放内存
+	free(ps->arr);
+	ps->arr = NULL;
+	free(ps);
+	ps = NULL;
 
 	return 0;
 }
+//struct S
+//{
+//	int n;
+//	int arr[0];//位置大小的- 柔性数组成员-数组的大小是可以调整的
+//};
+//int main()
+//{
+//	/*struct S s;
+//	printf("%d \n", sizeof(s));*/
+//	struct S* ps = (struct S*)malloc(sizeof(struct S) + 5 * sizeof(int));
+//	ps->n = 100;
+//	int i = 0;
+//	for (i = 0; i < 5; i++)
+//	{
+//		ps->arr[i] = i;
+//	}
+//	struct S* ptr = realloc(ps, 44);
+//	if (ptr !=NULL)
+//	{
+//		ps = ptr;
+//	}
+//	for (i=5; i<10; i++)
+//	{
+//		ps->arr[i] = i;
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", ps->arr[i]);
+//	}
+//	//释放
+//	free(ps);
+//	ps = NULL;
+//	return 0;
+//}
+//int main()
+//{
+//	int* p = (int*)calloc(10, sizeof(int));
+//	if (p == NULL)
+//	{
+//		printf("%s\n", strerror(errno));
+//
+//	}
+//	else
+//	{
+//		int i = 0;
+//		for (i = 0; i < 10; i++)
+//		{
+//			printf("%d", *(p + i));
+//		}
+//	}
+//	//释放空间
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
+//int main()
+//{
+//	//现在向内存申请10个整型的空间
+//	int* p = (int*)malloc(10 * sizeof(int));
+//	if (p == NULL)
+//	{
+//		//打印错误原因的一个方式
+//		printf("%s\n", strerror(errno));
+//	}
+//	else
+//	{
+//		//正常使用空间
+//		int i = 0;
+//		for (i = 0; i < 10; i++)
+//		{
+//			*(p + i) = i;
+//		}
+//		for (i = 0; i < 10; i++)
+//		{
+//			printf("%d ", *(p + i));
+//		}
+//	}
+//	//当动态申请的空间不再使用的时候
+//	//就应该还给操作系统
+//	//free（p）(谁开辟谁释放)
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
+//struct S
+//{
+//	char name[20];
+//	int age;
+//};
+//int main()
+//{
+//	struct S arr[50];//50个struct S类型的数据
+//	return 0;
+//}
+
+//C语言可以创建变长数组 - C99中增加了
